4bba22de1610cd6918ba2dabe98ab4cd64951ad5
Phillip Kim <philkim78@gmail.com>
2021-05-10 18:37:07 +1000

    Added code to print out all text from Powerpoint files

---
+++ search-files-directory.py
@@ -0,0 +1,17 @@
+from pptx import Presentation
+import glob
+
+#for eachfile in glob.glob("/Users/phillip.kim/Box/CV Blitz/[J-Z]*.pptx"):
+for eachfile in glob.glob("/Users/phillip.kim/Box/CV Blitz/*.pptx"):
+    try:
+        prs = Presentation(eachfile)
+        print(eachfile)
+        print("----------------------")
+        for slide in prs.slides:
+            for shape in slide.shapes:
+                if hasattr(shape, "text"):
+                    print(shape.text)
+    except:
+        print(eachfile)
+        print("ERROR")
+        print("----------------------")
