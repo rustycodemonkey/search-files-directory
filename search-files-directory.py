90be0f1195869ea52a73cb5090e2ba4059b84668
phillip.kim <philkim78@gmail.com>
2024-04-16 18:07:30 +1000

    Changed input directory as Box mount has changed and folder name has changed where one pager CVs are kept.

--- search-files-directory.py
+++ search-files-directory.py
@@ -1,8 +1,8 @@
 from pptx import Presentation
 import glob
 
-#for eachfile in glob.glob("/Users/phillip.kim/Box/CV Blitz/[J-Z]*.pptx"):
-for eachfile in glob.glob("/Users/phillip.kim/Box/CV Blitz/*.pptx"):
+# for eachfile in glob.glob("/Users/phillip.kim/Box/CV Blitz/[J-Z]*.pptx"):
+for eachfile in glob.glob("/Users/phillip.kim/Library/CloudStorage/Box-Box/CV Bios/*.pptx"):
     try:
         prs = Presentation(eachfile)
         print(eachfile)
